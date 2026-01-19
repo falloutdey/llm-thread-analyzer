d98c2a5f2799c1a1558ca569cfcad91627eb2ead
Deydson Costa <103591633+falloutdey@users.noreply.github.com>
2026-01-19 17:49:34 -0300

    configurando

---
+++ src/main/java/com/llm_thread_analyzer/LlmThreadAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.llm_thread_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmThreadAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmThreadAnalyzerApplication.class, args);
+	}
+
+}
