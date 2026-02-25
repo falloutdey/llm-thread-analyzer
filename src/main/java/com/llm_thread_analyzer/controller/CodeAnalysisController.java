a2069a727563bbc5cde37b10651d24de2cdc2ebe
Deydson Costa <103591633+falloutdey@users.noreply.github.com>
2026-02-25 12:50:46 -0300

    Refatora CodeAnalysisController para corrigir a injeção de dependência e reativa a rota de análise de código

--- src/main/java/com/llm_thread_analyzer/controller/CodeAnalysisController.java
+++ src/main/java/com/llm_thread_analyzer/controller/CodeAnalysisController.java
@@ -3,12 +3,13 @@ package com.llm_thread_analyzer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.llm_thread_analyzer.model.SourceCode;
+import com.llm_thread_analyzer.model.CodeAnalysisResults;
 import com.llm_thread_analyzer.service.CodeAnalysisService;
 
 @RestController
@@ -16,13 +17,18 @@ import com.llm_thread_analyzer.service.CodeAnalysisService;
 @CrossOrigin(origins = "*")
 public class CodeAnalysisController {
     
-    // @Autowired
-    // private CodeAnalysisService codeAnalysisService;
+    @Autowired
+    private CodeAnalysisService codeAnalysisService;
 
-    // @PutMapping("/arquivos/conteudo")
-    // public ResponseEntity<String> receberCodigoTeste(@RequestBody SourceCode sourceCode) {
-    //     codeAnalysisService.testarConexao();
-    //     return ResponseEntity.ok("Código Recebido com sucesso");
-    // }
-
+    // Rota oficial para receber o código do aluno e iniciar a análise
+    @PostMapping("/analisar")
+    public ResponseEntity<?> analisarCodigoThread(@RequestBody SourceCode sourceCode) {
+        try {
+            // Chama o serviço que orquestra a compilação, o SpotBugs e o LLM
+            CodeAnalysisResults resultados = codeAnalysisService.analisarCodigo(sourceCode);
+            return ResponseEntity.ok(resultados);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro ao analisar o código: " + e.getMessage());
+        }
+    }
 }
